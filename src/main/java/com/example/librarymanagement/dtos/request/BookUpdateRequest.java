98d538a21f3731aa421ce374c21b4bca58a9672e
NHTD <nhtd7502@gmail.com>
2025-01-20 14:04:52 +0700

    Create BookUpdateRequest.

---
+++ src/main/java/com/example/librarymanagement/dtos/request/BookUpdateRequest.java
@@ -0,0 +1,33 @@
+package com.example.librarymanagement.dtos.request;
+
+import com.example.librarymanagement.enums.BookStatusEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BookUpdateRequest {
+    Long id;
+
+    @JsonProperty("title")
+    String title;
+
+    @JsonProperty("description")
+    String description;
+
+    @JsonProperty("image")
+    String image;
+
+    @JsonProperty("category_id")
+    Long categoryId;
+
+    @JsonProperty("user_id")
+    String userId;
+
+    @JsonProperty("status")
+    BookStatusEnum status;
+}
